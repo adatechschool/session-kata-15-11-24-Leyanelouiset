9369d35f66d81b6ffc17bedca45a201c919dce62
leyanelouiset <louisetleyane@gmail.com>
2024-11-15 12:38:00 +0100

    exercice sauvegarde etp2

---
+++ index.js
@@ -0,0 +1,47 @@
+
+
+
+function getLatinCharacterList (texte){
+    var tableau = texte.split('')
+    
+console.log(tableau)
+}
+
+getLatinCharacterList("hello word")
+
+
+const latinToMorse = {
+	'A':'.-',
+	'B':'-...',
+	'C':'-.-.',
+	'D':'-..',
+	'E':'.',
+	'F':'..-.',
+	'G':'--.',
+	'H':'....',
+	'I':'..',
+	'J':'.---',
+	'K':'-.-',
+	'L':'.-..',
+	'M':'--',
+	'N':'-.',
+	'O':'---',
+	'P':'.--.',
+	'Q':'--.-',
+	'R':'.-.',
+	'S':'...',
+	'T':'-',
+	'U':'..-',
+	'V':'...-',
+	'W':'.--',
+	'X':'-..-',
+	'Y':'-.--',
+	'Z':'--..'
+}
+
+
+function translateLatinCharacter(latinToMorse){
+    for(let i =0;i<= 26;i++);
+    
+ }
+ 
